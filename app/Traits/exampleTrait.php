52a8b9e514aba3582a559ef55779576a7dc812a8
Tim Slager <timtendo12@gmail.com>
2022-12-07 14:48:45 +0100

    added example trait comment

--- app/Traits/exampleTrait.php
+++ app/Traits/exampleTrait.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Http\Request;
 
+//Dit is een trait, Zie het als een groep functies die je kan gebruiken in alle controllers met "$this->exampleFunction()";
+
 trait exampleTrait {
     function exampleFunction(): string
     {
