208f4cef0f2a83aca4bb76656325512a9fa1ef01
Tim Slager <timtendo12@gmail.com>
2023-01-11 14:23:47 +0100

    database migrations

--- database/migrations/2023_01_11_130458_create_user_roles_table.php
+++ database/migrations/2023_01_11_130458_create_user_roles_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->smallInteger('userType')->default(1);
-            $table->string("APIToken")->unique()->nullable();
-            $table->rememberToken();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('role_id')->references('id')->on('roles');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_roles');
     }
 };
