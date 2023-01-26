1571fe0bfcf312e8abb10370ec2320b58ebc5e07
Tim Slager <timtendo12@gmail.com>
2023-01-26 14:02:26 +0100

    added function to get user from token

--- app/Traits/tokenTrait.php
+++ app/Traits/tokenTrait.php
@@ -24,18 +24,28 @@ trait tokenTrait {
      * @param $returnUser boolean If true, the user will be returned
      * @return boolean|User Returns true if the token is valid, false if not. If $returnUser is true, the user will be returned.
      */
-    public function checkToken(string $token, int $userID, bool $returnUser = false): User|bool
+    public function checkToken(string $token, int $userID): User|bool
     {
-
         //checks if user exists with the given ID where the token is the same as the given token
         if(User::all()->where("id", $userID)->where("token", $token)->count() > 0){
             //if returnUser is true, return the user else return boolean
-            if ($returnUser) {
-                return User::all()->where("id", $userID)->where("token", $token)->first();
-            } else {
                 return true;
             }
-        }
+        //User does not exist or token is invalid so always return false.
+        return false;
+    }
+
+    /**
+     * @param string $token Token to get the user object from
+     * @return User|bool Returns the user object if the token is valid, false if not.
+     */
+    public function getUserFromToken(string $token): User|bool
+    {
+        //checks if user exists with the given ID where the token is the same as the given token
+        if(User::all()->where("token", $token)->count() > 0){
+            //if returnUser is true, return the user else return boolean
+                return User::all()->where("token", $token)->first();
+            }
         //User does not exist or token is invalid so always return false.
         return false;
     }
