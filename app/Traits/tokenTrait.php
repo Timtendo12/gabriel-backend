3f8c6b7d1f79ee8135af5c880fd3292f3d4b415a
Tim Slager <timtendo12@gmail.com>
2023-02-02 11:24:05 +0100

    updated phpdoc of tokenTrait

--- app/Traits/tokenTrait.php
+++ app/Traits/tokenTrait.php
@@ -11,9 +11,9 @@ trait tokenTrait {
     /**
      * Returns a generated token.
      * @return string
-     * @throws Exception if an appropriate source of randomness cannot be found.
+     * @throws Exception if an appropriate source of randomness cannot be found. (throw from random_bytes)
      */
-    public function generateToken()
+    public function generateToken(): string
     {
         return bin2hex(random_bytes(16));
     }
@@ -21,9 +21,9 @@ trait tokenTrait {
     /**
      * @param $token String APIToken to check
      * @param $userID int ID of the user to check
-     * @return boolean|User Returns true if the token is valid, false if not. If $returnUser is true, the user will be returned.
+     * @return boolean Returns true if the token is valid, false if not.
      */
-    public function checkToken(string $token, int $userID): User|bool
+    public function checkToken(string $token, int $userID): bool
     {
         //checks if user exists with the given ID where the token is the same as the given token
         if(User::all()->where("id", $userID)->where("token", $token)->count() > 0){
@@ -36,7 +36,7 @@ trait tokenTrait {
 
     /**
      * @param string $token Token to get the user object from
-     * @return User|bool Returns the user object if the token is valid, false if not.
+     * @return User|bool Returns the user object if the token is valid, false (bool) if not.
      */
     public function getUserFromToken(string $token): User|bool
     {
